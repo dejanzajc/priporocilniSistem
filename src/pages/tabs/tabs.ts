ba0103a13818854a37890dc52ecd818aaadab7e2
Dejan Zajc <dejan.zajc@infovizija.com>
2018-07-21 15:50:18 +0200

    Add workout, calendar, nutrition and profile page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { WorkoutPage } from "../workout/workout";
+import { CalendarPage } from '../calendar/calendar';
+import { NutritionPage } from "../nutrition/nutrition";
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = WorkoutPage;
+  tab2Root = CalendarPage;
+  tab3Root = NutritionPage;
+  tab4Root = ProfilePage;
 
   constructor() {
 
